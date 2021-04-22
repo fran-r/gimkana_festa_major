0f77be77931647db731e26c2e99a7332f8571263
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-04-22 13:07:43 +0200

    Update db scripts

---
+++ initialize_db.sh
@@ -0,0 +1,8 @@
+set -e
+
+source /fmrm/venv/bin/activate
+cd /fmrm/gimkana_festa_major
+rm db.sqlite3
+python3 manage.py migrate
+python3 manage.py loaddata gimkana/fixtures/users.json
+python3 manage.py loaddata gimkana/fixtures/qrs.yaml
--- update_qrs.sh
+++ update_qrs.sh
@@ -2,8 +2,4 @@ set -e
 
 source /fmrm/venv/bin/activate
 cd /fmrm/gimkana_festa_major
-rm db.sqlite3
-python3 manage.py migrate
-python3 manage.py loaddata gimkana/fixtures/users.json
 python3 manage.py loaddata gimkana/fixtures/qrs.yaml
-
