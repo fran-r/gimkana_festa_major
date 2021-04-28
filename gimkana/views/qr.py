79c13089123d61b93f8647d183c7f16d1c0cbe4f
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-04-28 11:17:20 +0200

    Fix is_scanned class on QRs list view

--- gimkana/views/qr.py
+++ gimkana/views/qr.py
@@ -1,4 +1,4 @@
-from django.db.models import BooleanField, IntegerField, Value, Case, When
+from django.db.models import BooleanField, IntegerField, Value, Case, When, Q
 from django.views.generic import DetailView, ListView
 
 from auth.SignupRequiredMixin import SignupRequiredMixin
@@ -14,7 +14,13 @@ class QrScannedListView(SignupRequiredMixin, ListView):
     template_name = 'gimkana/qr_scanned_list.html'
 
     def get_queryset(self):
-        user_scans = UserQr.objects.filter(user=self.request.user).values_list('qr_id')
+        user_scans = (
+            UserQr.objects
+            .filter(user=self.request.user)
+            .filter(scan_date__isnull=False)
+            .values_list('qr_id')
+        )
+
         return (
             Qr.objects.annotate(
                 is_scanned=Case(When(id__in=user_scans, then=Value(True)), default=Value(False),
