aab0060fabb50f1a1426ae3538ac6d1dc709445e
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-05-06 17:33:41 +0200

    Fix score count

--- gimkana/context_processors.py
+++ gimkana/context_processors.py
@@ -12,15 +12,15 @@ def scoring(request):
         .filter(user=request.user, scan_date__isnull=False)
         .aggregate(
             num_scanned=Count('*'),
-            num_qrs=Count('is_shop'),
+            num_shops=Sum('is_shop'),
             num_hints=Sum('hints'),
             score=Sum('value'),
         )
     )
 
     return {
-        'num_qrs': result['num_qrs'] or 0,
+        'num_qrs': (result['num_scanned'] - result['num_shops']) or 0,
         'num_hints': result['num_hints'] or 0,
-        'num_shops': (result['num_scanned'] - result['num_qrs']) or 0,
+        'num_shops': result['num_shops'] or 0,
         'score': result['score'] or 0,
     }
