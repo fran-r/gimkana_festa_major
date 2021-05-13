2adde614745a79e113da94c7245f8f5fec7b8f40
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-05-13 12:17:09 +0200

    Comment

--- auth/SignupRequiredMixin.py
+++ auth/SignupRequiredMixin.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+# This is unnecessary. Could have been solved in settings.LOGIN_URL
 class SignupRequiredMixin(LoginRequiredMixin):
     login_url = 'signup'
