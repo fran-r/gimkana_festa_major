630376d47b5ee071e36b32a434cb842c2c41c29a
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-05-23 23:24:08 +0200

    Fix IsStartedMixin

--- gimkana/mixins/is_started_mixin.py
+++ gimkana/mixins/is_started_mixin.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.mixins import AccessMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
 
 from gimkana.utils import is_started
 
 
-class IsStartedMixin(AccessMixin):
-    if not is_started():
-        template_name = 'gimkana/not_started.html'
+class IsStartedMixin(UserPassesTestMixin):
+    def test_func(self):
+        return is_started()
+
+    def handle_no_permission(self):
+        return redirect('not-started')
