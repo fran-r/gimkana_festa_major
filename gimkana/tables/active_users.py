6fd1f6ec442c9b8288a3d2b50c61bbaf17023cbd
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-05-23 22:24:51 +0200

    Format users table

--- gimkana/tables/active_users.py
+++ gimkana/tables/active_users.py
@@ -6,7 +6,7 @@ from gimkana.models.userqr import UserQr
 
 class UsersTable(Table):
     user__username = Column(verbose_name='Usuario')
-    user__email = Column(verbose_name='email')
+    user__email = Column(verbose_name='email', visible=False)
     score = Column(verbose_name='Puntos')
     num_qrs = Column(verbose_name='QRs')
     num_shops = Column(verbose_name='Comercios')
@@ -14,5 +14,6 @@ class UsersTable(Table):
 
     class Meta:
         model = UserQr
+        template_name = 'django_tables2/bootstrap-responsive.html'
         fields = ('user__username', 'user__email', 'score', 'num_qrs', 'num_shops', 'num_hints')
         ordering = ['score', 'num_scanned']
