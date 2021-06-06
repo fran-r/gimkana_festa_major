b89d7f6cc72597b8d434f6964c134576c8963219
fmrm <franciscomanuel.renesesminguez@telefonica.com>
2021-06-06 11:06:56 +0200

    Automatic signup and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,25 @@
+from datetime import datetime
+
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 from .forms import SignUpForm
 
 
+def signup(request):
+    username_str = '{}@gimkana.fm'.format(str(datetime.now()))
+    password = 'G1mkana$'
+    user = User.objects.create_user(username_str, username_str, password)
+    user.save()
+
+    user = authenticate(username=username_str, password=password)
+    login(request, user)
+    next_url = request.GET.get('next', '/')
+    return redirect(next_url)
+
+
+"""
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -18,3 +34,4 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+"""
